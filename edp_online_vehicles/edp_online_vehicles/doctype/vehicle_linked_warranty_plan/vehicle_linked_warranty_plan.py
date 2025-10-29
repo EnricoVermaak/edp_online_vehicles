27a2cf5c380e76b21323b7d31120cfe6876513d1
MC-Wolmarans <mc@tecwise.co.za>
2025-10-29 22:20:46 +0200

    Update vehicle_linked_warranty_plan.py

--- edp_online_vehicles/edp_online_vehicles/doctype/vehicle_linked_warranty_plan/vehicle_linked_warranty_plan.py
+++ edp_online_vehicles/edp_online_vehicles/doctype/vehicle_linked_warranty_plan/vehicle_linked_warranty_plan.py
@@ -7,28 +7,22 @@ from frappe.model.document import Document
 
 class VehicleLinkedWarrantyPlan(Document):
 	def after_insert(self):
-		"""Add the warranty plan to the Vehicle Stock's warranty table"""
 		self.add_to_vehicle_stock_warranty_table()
 	
 	def on_update(self):
-		"""Update Vehicle Stock warranty table if warranty plan changed"""
 		if self.has_value_changed("warranty_plan"):
-			# If warranty plan changed, we need to update both old and new records
-			# This is handled by the add_to_vehicle_stock_warranty_table method
+
 			self.add_to_vehicle_stock_warranty_table()
 	
 	def on_trash(self):
-		"""Remove the warranty plan from Vehicle Stock's warranty table"""
 		self.remove_from_vehicle_stock_warranty_table()
 	
 	def add_to_vehicle_stock_warranty_table(self):
-		"""Add warranty plan to Vehicle Stock's table_pcgj"""
 		if not self.vin_serial_no or not self.warranty_plan:
 			return
 		
 		vehicle_stock = frappe.get_doc("Vehicle Stock", self.vin_serial_no)
 		
-		# Check if warranty plan already exists in the table
 		warranty_exists = any(
 			row.warranty_plan_description == self.warranty_plan
 			for row in vehicle_stock.table_pcgj
@@ -42,13 +36,11 @@ class VehicleLinkedWarrantyPlan(Document):
 			frappe.db.commit()
 	
 	def remove_from_vehicle_stock_warranty_table(self):
-		"""Remove warranty plan from Vehicle Stock's table_pcgj"""
 		if not self.vin_serial_no or not self.warranty_plan:
 			return
 		
 		vehicle_stock = frappe.get_doc("Vehicle Stock", self.vin_serial_no)
 		
-		# Remove matching warranty plan from table
 		vehicle_stock.table_pcgj = [
 			row for row in vehicle_stock.table_pcgj
 			if row.warranty_plan_description != self.warranty_plan
