32e97be0e472cec4cf3811e98b77ab54a53b3b77
Ahmad-code-boy <ahmadsaeefgg@gmail.com>
2025-11-20 23:46:18 +0500

    change in service shedule for part childtable

--- edp_online_vehicles/edp_online_vehicles/doctype/service_schedules/service_schedules.js
+++ edp_online_vehicles/edp_online_vehicles/doctype/service_schedules/service_schedules.js
@@ -23,114 +23,74 @@ frappe.ui.form.on("Service Schedules", {
 frappe.ui.form.on('Service Schedules Parts Items', {
 
     item: function (frm, cdt, cdn) {
-        calculate_item_total(frm, cdt, cdn);
+        calculate_price(frm, cdt, cdn);
     },
 
     qty: function (frm, cdt, cdn) {
-        calculate_item_total(frm, cdt, cdn);
+        calculate_price(frm, cdt, cdn);
     }
 
 });
 
-// ----------------------------
-// FUNCTION: Calculate price & total
-// ----------------------------
-function calculate_all_items(frm) {
-    frm.doc.service_parts_items.forEach(row => {
-        if (!row.item) return;
+function calculate_price(frm, cdt, cdn) {
 
-        frappe.db.get_list('Item Price', {
-            filters: { item_code: row.item, price_list: 'Standard Selling' },
-            limit: 1,
-            fields: ['price_list_rate']
-        }).then(prices => {
-            let standard_rate = prices.length ? prices[0].price_list_rate : 0;
+    let row = locals[cdt][cdn];
+    if (!row.item) return;
 
-            frappe.db.get_doc('Item', row.item).then(item_doc => {
-                let custom_gp = item_doc.custom_service_gp || 0;
+    // --- 1) Get Standard Selling Price ---
+    frappe.db.get_list('Item Price', {
+        filters: { 
+            item_code: row.item,
+            price_list: 'Standard Selling'
+        },
+        limit: 1,
+        fields: ['price_list_rate']
+    }).then(prices => {
 
-                let price_excl = standard_rate * custom_gp;
-                frappe.model.set_value('Service Schedules Parts Items', row.name, 'price_excl', price_excl);
+        let standard_rate = prices.length ? prices[0].price_list_rate : 0;
 
-                let total = price_excl * (row.qty || 0);
-                frappe.model.set_value('Service Schedules Parts Items', row.name, 'total_excl', total);
+        // --- 2) Get Item Doc to read custom GP ---
+        frappe.db.get_doc('Item', row.item).then(item_doc => {
 
-                frm.refresh_field("service_parts_items");
-            });
+            let custom_gp = item_doc.custom_service_gp || 0;
+
+            // --- 3) Price & Total Calculation ---
+            let price = standard_rate * custom_gp;
+            let total = price * (row.qty || 0);
+
+            // --- 4) Set Values in Child Row ---
+            frappe.model.set_value(cdt, cdn, 'price_excl', price);
+            frappe.model.set_value(cdt, cdn, 'total_excl', total);
+
+            frm.refresh_field('service_parts_items');
+
         });
     });
 }
 
-
-// frappe.ui.form.on('Service Schedules Labour Items', {
-
-//     item: function (frm, cdt, cdn) {
-//         calculate_labour_total(frm, cdt, cdn);
-//     },
-
-//     duration_hours: function (frm, cdt, cdn) {
-//         calculate_labour_total(frm, cdt, cdn);
-//     }
-
-// });
-
-// // ---------------------------------
-// // FUNCTION: Calculate Rate & Total
-// // ---------------------------------
-// function calculate_labour_total(frm, cdt, cdn) {
-//     let row = locals[cdt][cdn];
-
-//     if (!row.item) return;
-
-//     frappe.db.get_list('Item Price', {
-//         filters: { item_code: row.item, price_list: 'Standard Selling' },
-//         limit: 1,
-//         fields: ['price_list_rate']
-//     }).then(prices => {
-
-//         let standard_rate = prices.length ? prices[0].price_list_rate : 0;
-
-//         frappe.db.get_doc('Item', row.item).then(item_doc => {
-
-//             let custom_gp = item_doc.custom_warranty_gp || 0;
-
-//             // Calculate rate_hour
-//             let rate_hour = standard_rate * custom_gp;
-//             frappe.model.set_value(cdt, cdn, 'rate_hour', rate_hour);
-
-//             // Calculate total_excl = duration_hours × rate_hour
-//             let total = (row.duration_hours || 0) * rate_hour;
-// 			console.log(total);
-			
-//             frappe.model.set_value(cdt, cdn, 'total_excl', total);
-
-//             frm.refresh_field("service_labour_items");
-//         });
-//     });
-// }
-
-
-
 frappe.ui.form.on('Service Schedules Labour Items', {
+	item: function (frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		if (!row.item) return;
+		
 
-    item: function (frm, cdt, cdn) {
-        calculate_total(frm, cdt, cdn);
-    },
+		frappe.db.get_list('Item Price', {
+			filters: { item_code: row.item, price_list: 'Standard Selling' },
+			limit: 1,
+			fields: ['price_list_rate']
+		}).then(prices => {
+			let standard_rate = prices.length ? prices[0].price_list_rate : 0;
 
-    duration_hours: function (frm, cdt, cdn) {
-        calculate_total(frm, cdt, cdn);
-    },
+			frappe.db.get_doc('Item', row.item).then(item_doc => {
+				let custom_gp = item_doc.custom_warranty_gp || 0;
+				let price = standard_rate * custom_gp;
 
-    rate_hour: function (frm, cdt, cdn) {
-        calculate_total(frm, cdt, cdn);
-    }
-
+				frappe.model.set_value(cdt, cdn, 'rate_hour', price);
+				frm.refresh_field('service_labour_items'); 
+			});
+		});
+	}
 });
 
-function calculate_total(frm, cdt, cdn) {
-    let row = locals[cdt][cdn];
 
-    let total = (row.duration_hours || 0) * (row.rate_hour || 0);
 
-    frappe.model.set_value(cdt, cdn, 'total_excl', total);
-}
