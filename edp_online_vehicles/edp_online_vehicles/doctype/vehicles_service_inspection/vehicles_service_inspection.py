937160b65038b13373b31df369379a1bd87e423d
MC-Wolmarans <mc@tecwise.co.za>
2026-01-05 14:31:16 +0200

    current hours fix

--- edp_online_vehicles/edp_online_vehicles/doctype/vehicles_service_inspection/vehicles_service_inspection.py
+++ edp_online_vehicles/edp_online_vehicles/doctype/vehicles_service_inspection/vehicles_service_inspection.py
@@ -9,10 +9,10 @@ from frappe.model.document import Document
 class VehiclesServiceInspection(Document):
 	def on_submit(self):
 		doc = frappe.get_doc("Vehicle Stock", self.vin_serial_no)
-		previous_hours = doc.current_hours
-		doc.current_hours = self.odo_reading
+		previous_hours = doc.last_service_hours
+		doc.last_service_hours = self.odo_reading
 		self_reference = frappe.utils.get_link_to_form(self.doctype, self.name)
-		msg = f"Odo Reading updated by vehicles Service Inspection {self_reference} done on {self.inspection_date} from {previous_hours} to {doc.current_hours}"
+		msg = f"Odo Reading updated by vehicles Service Inspection {self_reference} done on {self.inspection_date} from {previous_hours} to {doc.last_service_hours}"
 		doc.add_comment("Comment", msg)
 		doc.save()
 		frappe.db.commit()
