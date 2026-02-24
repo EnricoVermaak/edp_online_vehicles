74f22860bd111ff107483c3bb9fa2900dc0151ce
MC-Wolmarans <mc@tecwise.co.za>
2026-02-24 10:26:35 +0200

    check dupliucate reserved docs

--- edp_online_vehicles/events/create_reserve_doc.py
+++ edp_online_vehicles/events/create_reserve_doc.py
@@ -6,37 +6,37 @@ def create_reserve_doc(
 	docnames, dealer, status, reserve_reason, reserve_from_date, reserve_to_date=None, customer=None
 ):
 	docnames = frappe.parse_json(docnames)
+	last_name = None
 
 	for docname in docnames:
-		new_doc = frappe.new_doc("Reserved Vehicles")
+		if frappe.db.exists("Reserved Vehicles", docname):
+			reserve_doc = frappe.get_doc("Reserved Vehicles", docname)
+		else:
+			reserve_doc = frappe.new_doc("Reserved Vehicles")
+			reserve_doc.vin_serial_no = docname
+			reserve_doc.insert(ignore_permissions=True)
 
-		new_doc.vin_serial_no = docname
-		new_doc.dealer = dealer
-
+		reserve_doc.dealer = dealer
 		if customer:
-			new_doc.customer = customer
-
-		new_doc.status = status
-		new_doc.reserve_reason = reserve_reason
-		new_doc.reserve_from_date = reserve_from_date
-
+			reserve_doc.customer = customer
+		reserve_doc.status = status
+		reserve_doc.reserve_reason = reserve_reason
+		reserve_doc.reserve_from_date = reserve_from_date
 		if reserve_to_date:
-			new_doc.reserve_to_date = reserve_to_date
-
-		new_doc.insert(ignore_permissions=True)
-		new_doc.save(ignore_permissions=True)
+			reserve_doc.reserve_to_date = reserve_to_date
+		else:
+			reserve_doc.reserve_to_date = None
+		reserve_doc.save(ignore_permissions=True)
+		last_name = reserve_doc.name
 
 		stock_doc = frappe.get_doc("Vehicle Stock", docname)
-
 		stock_doc.availability_status = "Reserved"
 		stock_doc.reserve_reason = reserve_reason
-
 		stock_doc.save(ignore_permissions=True)
 		frappe.db.commit()
 
 	frappe.msgprint(f"Stock reserved for dealer {dealer}")
-
-	return new_doc.name
+	return last_name or (docnames[0] if docnames else None)
 
 
 @frappe.whitelist()
