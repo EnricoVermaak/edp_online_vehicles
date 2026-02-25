5d51134fdbc4726de0a262947cd178d9bf39a36a
MoniqueTecwise <monique@tecwise.co.za>
2026-02-25 10:51:30 +0200

    nix 7

--- edp_online_vehicles/edp_online_vehicles/page/part_order_1/point_of_sale.py
+++ edp_online_vehicles/edp_online_vehicles/page/part_order_1/point_of_sale.py
@@ -146,7 +146,7 @@ def get_items(start, page_length, price_list, item_group, search_term=""):
 		bin_join_selection = ", `tabBin` bin"
 		warehouses = ""
 		for wh in warehouse:
-			
+			warehouses = "f{}"
 		bin_join_condition = (
 			"AND bin.warehouse in (%(warehouses)s) AND bin.item_code = item.name AND bin.actual_qty > 0"
 		)
