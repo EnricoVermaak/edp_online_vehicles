2129f94253db04c493b2b66a3a266c57a46a6128
Vaughn-du-Preez <vaughn@tecwise.co.za>
2026-02-27 08:15:12 +0200

    Vehicle Shipment Status show but won't download

--- edp_online_vehicles/edp_online_vehicles/doctype/natis_errors/natis_errors.js
+++ edp_online_vehicles/edp_online_vehicles/doctype/natis_errors/natis_errors.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 // frappe.ui.form.on("Natis Errors", {
-// 	refresh(frm) {
-
-// 	},
+//     refresh: function(frm) {
+//         frm.add_custom_button("Import Natis Errors", function() {
+//             frappe.call({
+//                 method: "edp_online_vehicles_mahindra.events.natis_table_sync.populate_natis_errors",
+//                 callback: function(r) {
+//                     if (r.message) {
+//                         frappe.msgprint(r.message);
+//                         frm.reload_doc(); // refresh the form/list
+//                     }
+//                 }
+//             });
+//         });
+//     }
 // });
--- edp_online_vehicles/edp_online_vehicles/doctype/natis_errors/natis_errors.py
+++ edp_online_vehicles/edp_online_vehicles/doctype/natis_errors/natis_errors.py
@@ -1,9 +1,20 @@
-# Copyright (c) 2024, NexTash and contributors
-# For license information, please see license.txt
+# # Copyright (c) 2024, NexTash and contributors
+# # For license information, please see license.txt
 
+# # File: natis_errors/natis_errors/api.py
 # import frappe
-from frappe.model.document import Document
 
+# @frappe.whitelist()
+# def get_natis_errors(limit=50):
+#     # Direct query from your table
+#     data = frappe.db.sql(f"""
+#         SELECT 
+#             post_date, post_time, post_type, error_code, error_field,
+#             error_descr, stock_no, vin_no, brand_id, client_name,
+#             client_id_type, client_id_number, dealer_id, email_sent_on
+#         FROM natis_errors
+#         ORDER BY post_date DESC, post_time DESC
+#         LIMIT %s
+#     """, (limit,), as_dict=True)
 
-class NatisErrors(Document):
-	pass
+#     return data
