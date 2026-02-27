4816155bece0e392e65e569ce84975426072d372
Vaughn-du-Preez <vaughn@tecwise.co.za>
2026-02-27 11:05:40 +0200

    TW-6850

--- edp_online_vehicles/edp_online_vehicles/doctype/natis_errors/natis_errors.js
+++ edp_online_vehicles/edp_online_vehicles/doctype/natis_errors/natis_errors.js
@@ -2,7 +2,26 @@
 // For license information, please see license.txt
 
 // frappe.ui.form.on("Natis Errors", {
-// 	refresh(frm) {
+//     onload: function(frm) {
+//         // Only run if document is new or not yet populated
+//         if (frm.is_new() && !frm.doc.natis_errors_loaded) {
+//             frappe.call({
+//                 method: "edp_online_vehicles_mahindrasa.events.natis_table_sync.populate_natis_errors",
+//                 callback: function(r) {
+//                     if (r.message) {
+//                         frappe.msgprint(r.message);
 
-// 	},
+//                         // Mark as loaded so it doesn't run again
+//                         frm.set_value("natis_errors_loaded", true); // false for test DELETE later
+//                         frm.save();
+//                     }
+//                 }
+//             });
+//         }
+//         frappe.listview_settings["Natis Errors"] = {
+//         onload: function(listview) {
+//             listview.page.clear_primary_action();
+//         }
+//         };
+//     }
 // });
