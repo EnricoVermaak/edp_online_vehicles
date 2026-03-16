8c1b15eae05f2f2c4726c9eb69f235d0eaf89aab
gerhardTECWISE <gerhard@tecwise.co.za>
2026-03-16 16:57:25 +0200

    Implement item creation functionality and enhance Item Details DocType

--- edp_online_vehicles/edp_online_vehicles/doctype/item_details/item_details.js
+++ edp_online_vehicles/edp_online_vehicles/doctype/item_details/item_details.js
@@ -1,8 +1,33 @@
 // Copyright (c) 2026, NexTash and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Item Details", {
-// 	refresh(frm) {
+frappe.ui.form.on('Item Details', {
+    create_item_button(frm) {
+        frappe.call({
+            method: "create_item",
+            doc: frm.doc,
+            freeze: true,
+            freeze_message: "Creating Item...",
+            callback: function(r) {
+                frappe.msgprint("Item created");
+            }
+        });
+    },
+    onload(frm) {
+        frm.disable_save();
+        frm.page.clear_menu();
+        frm.set_value({
+            item_code: "",
+            item_description: "",
+            item_group: "",
+            bin_location: "",
+            uom: "",
+            service_gp: "",
+            warranty_gp: ""
+        });
 
-// 	},
-// });
+        frm.clear_table("table_item_price_lists");
+        frm.refresh_field("table_item_price_lists");
+    }
+});
+
